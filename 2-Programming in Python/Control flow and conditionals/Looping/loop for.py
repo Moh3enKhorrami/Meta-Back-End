9fbb82804ae4bbc4ddd0a32a5499b8ddbcb7740f
kh.moh3en <kh.moh3en@live.com>
2024-01-02 12:40:39 +0100

    Cheng

--- 2-Programming in Python/Control flow and conditionals/Looping/loop for.py
+++ 2-Programming in Python/Control flow and conditionals/Looping/loop for.py
@@ -1,9 +1,9 @@
 import time
 start_time = time.time()
 
-for i in range (10):
-    for j in range(10):
+for i in range (100):
+    for j in range(1000):
         print(0, end = " ")
     print()
     
-print(round((time.time() - strat_time ), 2))
+print(round((time.time() - start_time ), 2))
