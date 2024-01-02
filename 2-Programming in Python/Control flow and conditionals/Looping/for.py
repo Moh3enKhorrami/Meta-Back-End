ca2c7180c117c27192a079567638ea96445160cb
kh.moh3en <kh.moh3en@live.com>
2024-01-02 11:41:46 +0100

    for and while

---
+++ 2-Programming in Python/Control flow and conditionals/Looping/for.py
@@ -0,0 +1,8 @@
+favorites = ["Creme Brulee", "Apple Pie", "Churros", "Tiramisu", "Chocolate Cake"]
+
+for item in favorites:
+    print("I Like this desert ", item)
+
+# Print whit value
+for dex, item in enumerate(favorites):
+    print(dex, item)
---
+++ 2-Programming in Python/Control flow and conditionals/Looping/while.py
@@ -0,0 +1,6 @@
+favorites = ["Creme Brulee", "Apple Pie", "Churros", "Tiramisu", "Chocolate Cake"]
+
+count = 0
+while count < len(favorites):
+    print("I Like this desert ", favorites[count])
+    count += 1
