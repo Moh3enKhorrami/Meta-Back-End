64dd94838fc7043865f7cfac7a5ede574898a250
kh.moh3en <kh.moh3en@live.com>
2024-01-02 19:46:11 +0100

    update

--- 2-Programming in Python/Control flow and conditionals/Tuples/Update Tuples.py
+++ 2-Programming in Python/Control flow and conditionals/Tuples/Update Tuples.py
@@ -19,3 +19,15 @@ y = ("orange",)
 thistuple += y
 
 print(thistuple)
+
+
+#Remove Items
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+#2
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
