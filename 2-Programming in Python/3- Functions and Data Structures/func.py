01674d23d5933b7bcdee20b9aee794c319b5218d
kh.moh3en <kh.moh3en@live.com>
2024-01-03 10:55:02 +0100

    Example Function

---
+++ 2-Programming in Python/3- Functions and Data Structures/func.py
@@ -0,0 +1,20 @@
+def calcuate_tax(bill, tax_rate):
+    return round((bill * tax_rate) / 100, 2)
+
+
+print(calcuate_tax(164.50, 22))
+
+#Arbitrary Arguments, *args
+#If you do not know how many arguments that will be passed into your function, add a * before the parameter name in the function definition.
+#This way the function will receive a tuple of arguments, and can access the items accordingly:
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+
+#Keyword Arguments
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
