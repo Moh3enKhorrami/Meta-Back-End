b0afec7beb226f48ba80772ae53052f509f0c0ae
kh.moh3en <kh.moh3en@live.com>
2024-01-06 10:26:03 +0100

    test

---
+++ gmae.py
@@ -0,0 +1,20 @@
+import random
+a = 0
+b = 99
+hads = random.randint(a, b)
+print(hads)
+javab = str(input("Che fekri mikoni: "))
+
+while javab != :
+    if javab == b:
+        b = int(javab)
+        hads = random.randint(a, b)
+    elif javab == k:
+        a = int(javab)
+        hads = random.randint(a, b)
+    elif javab == d:
+        print("Dorost gofti: ", javab)
+
+hads = random.randint(a, b)
+print(hads)
+
